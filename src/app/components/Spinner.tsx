b8e45a7adbe9b583358f48653181f8311b7f60d1
dongwoo <114900672+BaeDongWoo@users.noreply.github.com>
2025-03-05 05:32:24 +0900

    http 호출 및 응답 데이터 상태 관리 (#8)
    * feat : 페이지 라우트
    
    * chore : react-query & axios install
    
    * feat : 포켓몬 리스트를 불러오는 hook
    
    * feat : 포켓몬 리스트 호출 및 출력
    
    * feat : useInfiniteQuery로 변경
    
    * feat : 무한 스크롤용 트리거용 ref추가
    
    * feat : 데이터 로딩용 spinner
    
    * refactor : 무한 스크롤 코드 분리

---
+++ src/app/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div
+      className={`border-4 border-t-transparent border-solid rounded-full animate-spin w-12 h-12 border-blue-500 m-auto`}
+    ></div>
+  );
+};
