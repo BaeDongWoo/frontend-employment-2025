6e0969ea82c6cc273265f4fe913fe18990f037d1
BaeDongWoo <ehddn5410@naver.com>
2025-03-04 21:20:29 +0900

    feat : 버튼 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { TanstackQueryProvider } from '@/configs/tanstack-query/provider';
+import { CountButton } from './components/CountButton';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TanstackQueryProvider>{children}</TanstackQueryProvider>
+        <CountButton />
       </body>
     </html>
   );
