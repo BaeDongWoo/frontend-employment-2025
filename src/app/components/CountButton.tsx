a623904567d6c7fff5c93ad73fb6ff0d5c24ba42
BaeDongWoo <ehddn5410@naver.com>
2025-03-05 01:30:11 +0900

    feat: isMode 상태에 따른 dark 클래스 추가

--- src/app/components/CountButton.tsx
+++ src/app/components/CountButton.tsx
@@ -25,19 +25,25 @@ export const CountButton = () => {
     if (count >= 5) {
       setIsMode(true);
       localStorage.setItem('isMode', 'dark');
+      document.documentElement.classList.add('dark');
     } else if (count < 5) {
       setIsMode(false);
       localStorage.setItem('isMode', 'white');
+      document.documentElement.classList.remove('dark');
     }
   }, [count]);
 
   return (
-    <div className="fixed bottom-4 right-4 rounded-full bg-black p-2 flex flex-col justify-center items-center gap-2">
-      <button onClick={increment} className="w-10 h-10 rounded-full bg-blue-500 text-white hover:bg-blue-600">
+    <div
+      className={`fixed bottom-4 right-4 rounded-full ${
+        isMode ? 'bg-white' : 'bg-black'
+      } p-2 flex flex-col justify-center items-center gap-2`}
+    >
+      <button onClick={increment} className="w-10 h-10 rounded-full bg-blue-500 hover:bg-blue-600 text-white">
         ▲
       </button>
-      <p className="text-lg font-bold text-white">{count}</p>
-      <button onClick={decrement} className="w-10 h-10 rounded-full bg-red-500 text-white hover:bg-red-600">
+      <p className={`font-bold ${isMode ? 'text-black' : 'text-white'}`}>{count}</p>
+      <button onClick={decrement} className="w-10 h-10 rounded-full bg-red-500 hover:bg-red-600 text-white">
         ▼
       </button>
     </div>
